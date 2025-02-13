8319a796cdc9cd9e6b267bd8664640dd8fc6bc92
sbancuz <sbancuz@protonmail.com>
2025-02-14 00:40:45 +0100

    Fix

--- build.zig
+++ build.zig
@@ -4,11 +4,9 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    const dep_opts = .{ .target = target, .optimize = optimize };
-    _ = dep_opts;
-
     _ = b.addModule("throw", .{
-        .source_file = .{ .path = "src/throw.zig" },
-        .dependencies = &.{},
+        .root_source_file = b.path("src/throw.zig"),
+        .target = target,
+        .optimize = optimize,
     });
 }
